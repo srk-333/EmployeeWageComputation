d265d263e94d447d400c0cdfd424bf58692b4a29
srk-333 <sauravmk333@gmail.com>
2022-01-13 14:28:05 +0530

    [Saurav]Added Welcome message program

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class Program
+    {
+        // Main Method /Entry point
+        static void Main(string[] args)
+        {
+            //printing message on console
+            Console.WriteLine("Welcome To Employee Wage Computation Program");
+            Console.ReadLine();
+        }
+    }
+}
